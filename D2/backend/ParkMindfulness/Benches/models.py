0b9584c4f68cc5ec6104b73088b3e19a9b727784
Samantha Pang <samantha.pang@mail.utoronto.ca>
2023-03-04 09:42:38 -0500

    Audio Model set up with audio_files folder in media created

--- D2/backend/ParkMindfulness/Benches/models.py
+++ D2/backend/ParkMindfulness/Benches/models.py
@@ -13,3 +13,37 @@ class Benches(models.Model):
     #images 
     qr_code = models.ImageField(null=True, blank=True, upload_to="images/qr_codes/")
     thumbnail = models.ImageField(null=True, blank=True, upload_to="images/bench_thumbnails/")
+
+# Audio Model
+# Length_Category: The 1st value is the actual value and 
+# the 2nd value is displayed on Django Admin
+# https://stackoverflow.com/questions/18676156/how-to-properly-use-the-choices-field-option-in-django
+
+# bench_id: related_name = 'audios' allows for reverse relationship (get all audios related to one bench)
+# audio_binary: whether audio exists (True) or not (False)
+# audio_file: store under 'media/audio_files/'
+# length_category: 0-5, 5-10, >10 # TODO clarify categories with others 
+# contributor
+
+class Audio(models.Model):
+    """
+    Model representing an audio file that is associated with a bench.
+    """
+    class Length_Category(models.TextChoices):
+        SHORT = "0-5", "0-5 minutes"
+        MEDIUM = "5-10", "5-10 minutes"
+        LONG = ">10", "greater than 10 minutes"
+
+    audio_id = models.AutoField(primary_key=True) #PK 
+    bench_id = models.ForeignKey(Benches, on_delete=models.CASCADE, related_name='audios') #FK
+    audio_binary = models.BooleanField(default=False) 
+    audio_file = models.FileField(upload_to='audio_files/', blank=True, null=True) #optional
+    length_category = models.CharField(max_length=30, 
+                                       choices=Length_Category.choices,
+                                       blank=True, 
+                                       null=True) #optional 
+    contributor = models.CharField(max_length=100, blank=True, null=True) #optional 
+
+    def get_length_category(self): 
+        return Audio.Length_Category(self.length_category)
+    
