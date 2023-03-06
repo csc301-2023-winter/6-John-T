87743b57ec59c723df53aa49f326af64b5190df8
tshafiq <tajwaarshafiq@gmail.com>
2023-03-06 03:09:22 +0300

    Added CORS and CSRF origins to backend for frontend communication

--- D2/backend/ParkMindfulness/ParkMindfulness/settings.py
+++ D2/backend/ParkMindfulness/ParkMindfulness/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     "rest_framework",
     "greg",
     "Benches",
+    "corsheaders"
 ]
 
 MIDDLEWARE = [
@@ -51,6 +52,20 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "corsheaders.middleware.CorsMiddleware"
+]
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://6-john-t-one.vercel.app/",
+    "https://6-john-t-production.up.railway.app/"
+    # TODO: ADD FE MANAGER URL HERE
+]
+
+CORS_ORIGIN_ALLOW_ALL = False
+
+CORS_ORIGIN_WHITELIST = [
+    "https://6-john-t-one.vercel.app/"
+    # TODO: ADD FE MANAGER URL HERE
 ]
 
 ROOT_URLCONF = "ParkMindfulness.urls"
