02aaad5489db8b34661de3157c4ab873266ae102
THLi3 <31786494+THLi3@users.noreply.github.com>
2023-03-08 18:33:37 -0500

    url fix

--- D2/user-frontend/src/components/ScanPage/ScanPage.jsx
+++ D2/user-frontend/src/components/ScanPage/ScanPage.jsx
@@ -63,7 +63,7 @@ class Reader extends Component {
             let id = new URL(data.text.replace("#/", "")).searchParams;
             this.setState({
                 result: data,
-                url: data.text
+                url: data.text.split("/")[1]
             })
             fetch(`${BACKEND_URL}${BACKEND_PATH_FOR_BENCH_DETAILS}${id.get("m")}/`, {
                 method: 'GET',
