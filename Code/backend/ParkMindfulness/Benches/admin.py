50524e69ce08d6e5cf3a0a713de780e618f7be43
Samantha Pang <samantha.pang@mail.utoronto.ca>
2023-04-07 09:30:53 -0400

    Docstrings added to Benches app

--- Code/backend/ParkMindfulness/Benches/admin.py
+++ Code/backend/ParkMindfulness/Benches/admin.py
@@ -1,17 +1,32 @@
 from django.contrib import admin
 from .models import *
 
-# Register your models here.
+"""
+Register the models with the Django admin interface to manage benches. 
+"""
 
 @admin.register(Benches)
 class AdminProfile(admin.ModelAdmin):
+    """
+    Admin class for Benches.
+    """
     list_display = ['bench_id','bench_title', 'park_id', 'qr_code', 'thumbnail']
 
 @admin.register(Audio)
 class AdminProfile(admin.ModelAdmin):
+    """
+    Admin class for Audio.
+    """
     list_display = ['audio_id','bench_id', 'audio_binary', 'audio_file',
                     'contributor', 'length_category', 'season_category']
-    list_filter = ('bench_id', 'length_category', 'season_category') # allow filtering by bench id, length, season
+    list_filter = ('bench_id', 'length_category', 'season_category') 
+    """
+    Filters for `Audio` in the Django admin interface.
+    
+    - bench_id: Filter audio by `Bench` object.
+    - length_category: Filter audio by the length category.
+    - season_category: Filter audio by the season category.
+    """
 
 
     
